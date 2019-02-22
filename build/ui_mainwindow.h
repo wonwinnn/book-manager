9cf2fb5a7bc319896a9c2bcb98d154edb14c59b6
wonwinnn <46086085+wonwinnn@users.noreply.github.com>
2019-02-22 17:44:20 +0800

    Update

---
+++ build/ui_mainwindow.h
@@ -0,0 +1,151 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QTableView *BookView;
+    QPushButton *DelBtn;
+    QWidget *horizontalLayoutWidget_2;
+    QHBoxLayout *horizontalLayout_2;
+    QLineEdit *TitleEdit;
+    QPushButton *AddBtn;
+    QWidget *horizontalLayoutWidget_3;
+    QHBoxLayout *horizontalLayout_3;
+    QLineEdit *IsbnEdit;
+    QPushButton *SearchBtn;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(403, 384);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayoutWidget = new QWidget(centralWidget);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(20, 10, 361, 201));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        BookView = new QTableView(horizontalLayoutWidget);
+        BookView->setObjectName(QStringLiteral("BookView"));
+        BookView->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+        horizontalLayout->addWidget(BookView);
+
+        DelBtn = new QPushButton(horizontalLayoutWidget);
+        DelBtn->setObjectName(QStringLiteral("DelBtn"));
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(DelBtn->sizePolicy().hasHeightForWidth());
+        DelBtn->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(DelBtn);
+
+        horizontalLayoutWidget_2 = new QWidget(centralWidget);
+        horizontalLayoutWidget_2->setObjectName(QStringLiteral("horizontalLayoutWidget_2"));
+        horizontalLayoutWidget_2->setGeometry(QRect(20, 220, 361, 41));
+        horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget_2);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        TitleEdit = new QLineEdit(horizontalLayoutWidget_2);
+        TitleEdit->setObjectName(QStringLiteral("TitleEdit"));
+
+        horizontalLayout_2->addWidget(TitleEdit);
+
+        AddBtn = new QPushButton(horizontalLayoutWidget_2);
+        AddBtn->setObjectName(QStringLiteral("AddBtn"));
+        sizePolicy.setHeightForWidth(AddBtn->sizePolicy().hasHeightForWidth());
+        AddBtn->setSizePolicy(sizePolicy);
+
+        horizontalLayout_2->addWidget(AddBtn);
+
+        horizontalLayoutWidget_3 = new QWidget(centralWidget);
+        horizontalLayoutWidget_3->setObjectName(QStringLiteral("horizontalLayoutWidget_3"));
+        horizontalLayoutWidget_3->setGeometry(QRect(20, 270, 361, 41));
+        horizontalLayout_3 = new QHBoxLayout(horizontalLayoutWidget_3);
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(0, 0, 0, 0);
+        IsbnEdit = new QLineEdit(horizontalLayoutWidget_3);
+        IsbnEdit->setObjectName(QStringLiteral("IsbnEdit"));
+
+        horizontalLayout_3->addWidget(IsbnEdit);
+
+        SearchBtn = new QPushButton(horizontalLayoutWidget_3);
+        SearchBtn->setObjectName(QStringLiteral("SearchBtn"));
+
+        horizontalLayout_3->addWidget(SearchBtn);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 403, 25));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        DelBtn->setText(QApplication::translate("MainWindow", "Delete", 0));
+        AddBtn->setText(QApplication::translate("MainWindow", "Add", 0));
+        SearchBtn->setText(QApplication::translate("MainWindow", "Search", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
